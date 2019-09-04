15b6c1a54e70ce10e3f642ba215db8e9e9e3c381
Pano Papadatos <pano.papadatos@publicrelay.com>
2019-09-04 13:03:22 -0400

    Adding injected CSS and content script

---
+++ scripts/content.js
@@ -0,0 +1,19 @@
+/**
+ * This is the content script that will run in all yucata.de pages
+ */
+
+setupHotkeys();
+
+/**
+ * Set up the keypress handlers for all the hotkeys
+ */
+function setupHotkeys(){
+    console.log("I set up hotkeys!");
+    // document.body.onkeypress = (e) => {
+    //     if(e.key === "a"){
+    //         console.log("You clicked A!");
+    //         document.getElementById("btn_finishTurn").click();
+    //     }
+    //
+    // }
+}
