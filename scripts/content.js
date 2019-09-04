4df9d3f27ea7d32440de5d2aa7cefb3a5f368ebe
Pano Papadatos <pano.papadatos@publicrelay.com>
2019-09-04 13:53:29 -0400

    Adding working hotkey for finish turn and hotkey map

--- scripts/content.js
+++ scripts/content.js
@@ -2,19 +2,47 @@
  * This is the content script that will run in all yucata.de pages
  */
 
+const hotkeysMap = {
+    "a": {
+        description: "Hit the 'Finish Turn' button",
+        method: () => clickBySelector("#btn_finishTurn")
+    }
+};
+
 setupHotkeys();
+
 
 /**
  * Set up the keypress handlers for all the hotkeys
  */
 function setupHotkeys(){
-    console.log("I set up hotkeys!");
-    // Info on hotkeys https://developer.chrome.com/extensions/commands
-    // document.body.onkeypress = (e) => {
-    //     if(e.key === "a"){
-    //         console.log("You clicked A!");
-    //         document.getElementById("btn_finishTurn").click();
-    //     }
-    //
-    // }
+    console.log("I set up ze hotkeys!");
+
+    Object.keys(hotkeysMap).forEach(key => {
+        window.addEventListener("keypress", (e) => {
+            // If the key in the event matches the key we are setting up the handler for
+            if(e.key === key){
+                const data = hotkeysMap[key];
+                // If we have a binding for this hotkey
+                if(data){
+                    const {description, method} = data;
+                    console.log(`Action: ${description}`);
+                    method();
+                }
+            }
+        });
+    });
+}
+
+/**
+ * Click the element in the selector if it is found
+ * @param {string} selector
+ */
+function clickBySelector(selector){
+    const element = document.querySelector(selector);
+    if(element){
+        element.click();
+    } else {
+        console.log(`Element ${selector} not found`);
+    }
 }
