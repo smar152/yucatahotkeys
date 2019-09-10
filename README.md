1d27609c6fe869d71c6296174f03312faa226f83
Pano Papadatos <pano.papadatos@publicrelay.com>
2019-09-10 12:37:52 -0400

    Moving files

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## yucatahotkeys
 
 Keyboard shortcuts for yucata games
+
+Source Code for Development is in **build/src**
+
+Code for Deployment is in **build/dist**
--- scripts/dom.js
+++
@@ -1,7 +0,0 @@
-/**
- * Return the dom node of the button in the popup
- * @returns {HTMLElement}
- */
-export const getPopupButton = () =>{
-    return document.getElementById("popupButton");
-};
